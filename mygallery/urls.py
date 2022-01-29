794a6f470446c98d27862e9022a465c0593a5e86
Edwike12 <nyaunchoedwike@gmail.com>
2022-01-30 00:16:05 +0300

    added a path for app

--- mygallery/urls.py
+++ mygallery/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url,include
+from django.urls import path,include
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^gallery/',include('gallery.urls')),
+    path('admin/', admin.site.urls),
+    path('',include('gallery.urls')),
 ]
