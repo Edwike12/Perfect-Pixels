0e74c665dd41d0c2291d0fe737c391dedab52b6d
Edwike12 <nyaunchoedwike@gmail.com>
2022-01-30 19:24:58 +0300

    added an image class function

--- gallery/models.py
+++ gallery/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 # class location
 # class category
 # class image
+class Image(models.Model):
+    image=CloudinaryField('Image')
+    name=models.CharField(max_length=50)
+    description=models.TextField(max_length=1000)
