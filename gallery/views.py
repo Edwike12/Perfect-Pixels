aecf5132cd2027a4b732ce65ee926172a69c98a0
Edwike12 <nyaunchoedwike@gmail.com>
2022-01-30 19:26:20 +0300

    added a view function for the images

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,25 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+
+from gallery.forms import ImageForm
+from .models import *
 
 # Create your views here.
 def index(request):
-   return render(request, 'index.html')
+   post=Image.objects.all()
+
+   context={'post':post}
+   return render(request, 'index.html',context)
+
+def add_image(request):
+   
+   if request.method=='POST':
+      form = ImageForm (request.POST , request.FILES)
+      if form.is_valid():
+            form.instance.user = request.user
+            form.save()
+      return HttpResponseRedirect('/') 
+   else:
+      form=ImageForm()
+   return render(request, 'add_image.html',{'form':form})
+    
