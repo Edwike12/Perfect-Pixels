43411ef05c1eca564fbb8083e05e01845075a1a3
Edwike12 <nyaunchoedwike@gmail.com>
2022-01-31 02:15:00 +0300

    added path for search and images

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
    path('',views.index,name = 'index'),
    path('add_image',views.add_image, name='add_image'),
+   path('search/',views.search_results, name='search_results'),
+   path('images/',views.images, name='images'),
 ]
